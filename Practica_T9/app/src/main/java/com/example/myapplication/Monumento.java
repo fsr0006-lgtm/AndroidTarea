f561fe66d3552d94bb1eccaa7022b23b48b594b2
fsr0006-lgtm <fsr0006@alu.medac.es>
2026-01-12 17:29:17 +0100

    get y set clase monumento

--- Practica_T9/app/src/main/java/com/example/myapplication/Monumento.java
+++ Practica_T9/app/src/main/java/com/example/myapplication/Monumento.java
@@ -1,4 +1,55 @@
 package com.example.myapplication;
 
 public class Monumento {
+
+    private String nombre;
+    private String ubicacion;
+    private String descripcion;
+    private String imagen;
+
+    public Monumento(String nombre, String ubicacion, String descripcion, String imagen){
+
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+
+
+
+
+
+
 }
