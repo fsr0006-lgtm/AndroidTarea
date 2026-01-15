04bd243e899768bc39f4ecacc3a313819fdaecf4
fsr0006-lgtm <fsr0006@alu.medac.es>
2026-01-15 15:20:54 +0100

    adaptadormonumentos

--- Practica_T9/app/src/main/java/com/example/myapplication/AdaptadorMonumentos.java
+++ Practica_T9/app/src/main/java/com/example/myapplication/AdaptadorMonumentos.java
@@ -1,4 +1,9 @@
 package com.example.myapplication;
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -23,6 +28,8 @@ public class AdaptadorMonumentos extends Recyclerview.Adapter<AdaptadorMonumento
         }
     }
     private ArrayList<Monumento> monumentos;
+    private Context context;
+
 
     //Video minuto 6:54
 
