f0076324245d055fe1cef760d40ef5949dfe841c
fsr0006-lgtm <fsr0006@alu.medac.es>
2026-01-15 16:07:16 +0100

    mainacabado

--- Practica_T9/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Practica_T9/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,8 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvMonumentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +38,57 @@ public class MainActivity extends AppCompatActivity {
             return insets;
 
         });
+        rvMonumentos = findViewById(R.id.rvMonumentos);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "https (poner la url del laravel ahora despues)";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        ArrayList<Monumento> arrayMonumentos = new ArrayList<>();
+
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject jsonObject = response.getJSONObject(i);
+
+                                String nombre = jsonObject.getString("nombre");
+                                String ubicacion = jsonObject.getString("ciudad");
+                                String descripcion = jsonObject.getString("descripcion");
+                                String imagen = jsonObject.getString("imagen");
+
+                                Monumento monumento = new Monumento(nombre, ubicacion, descripcion, imagen);
+                                arrayMonumentos.add(monumento);
+                            }
+
+                            rvMonumentos.setHasFixedSize(true);
+
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+                            rvMonumentos.setLayoutManager(layoutManager);
+
+                            AdaptadorMonumentos adaptadorMonumentos = new AdaptadorMonumentos(MainActivity.this, arrayMonumentos);
+                            rvMonumentos.setAdapter(adaptadorMonumentos);
+                            adaptadorMonumentos.refrescar();
+
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+        queue.add(jsonArrayRequest);
+    
+
+
     }
 }
