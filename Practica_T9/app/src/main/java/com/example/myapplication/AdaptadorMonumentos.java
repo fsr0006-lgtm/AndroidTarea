e36bc38e3dc9ec8d363dcff57b82130b3334641e
fsr0006-lgtm <fsr0006@alu.medac.es>
2026-01-15 15:45:23 +0100

    TerminadoAdaptador

--- Practica_T9/app/src/main/java/com/example/myapplication/AdaptadorMonumentos.java
+++ Practica_T9/app/src/main/java/com/example/myapplication/AdaptadorMonumentos.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -8,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class AdaptadorMonumentos extends Recyclerview.Adapter<AdaptadorMonumentos.HolderMonumentos>{
+public class AdaptadorMonumentos extends RecyclerView.Adapter<AdaptadorMonumentos.HolderMonumentos>{
 
 
     public static class HolderMonumentos extends RecyclerView.ViewHolder {
@@ -29,6 +31,61 @@ public class AdaptadorMonumentos extends Recyclerview.Adapter<AdaptadorMonumento
     }
     private ArrayList<Monumento> monumentos;
     private Context context;
+    public AdaptadorMonumentos(Context context, ArrayList<Monumento>arrayMonumentos) {
+        this.context = context;
+        this.monumentos = arrayMonumentos;
+    }
+
+    public void add(ArrayList<Monumento> arrayMonumentos) {
+        monumentos.clear();
+        monumentos.addAll(arrayMonumentos);
+    }
+    public void refrescar(){
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public HolderMonumentos onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.monumentos, parent, false);
+        HolderMonumentos holder = new HolderMonumentos(itemView);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(HolderMonumentos holder, int position) {
+        String nombre = monumentos.get(position).getNombre();
+        String ubicacion = monumentos.get(position).getUbicacion();
+        String descripcion = monumentos.get(position).getDescripcion();
+        String imagen = monumentos.get(position).getImagen();
+
+        holder.tvNombre.setText(nombre);
+        holder.tvUbicacion.setText(ubicacion);
+        holder.tvDescripcion.setText(descripcion);
+
+        //Glide.with(holder.itemView.getContext()).load(imagen).into(holder.imagenMonumento);
+
+        if (position % 2 == 0) {
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.color1));
+        } else {
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.color2));
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return monumentos.size();
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
+
+
+
+
+
 
 
     //Video minuto 6:54
